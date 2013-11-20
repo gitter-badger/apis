43c4eeb3df1b6d8ffa1fde84394239f90e6cb30d
Sean Wellington <swellington@utest.com>
2013-11-20 16:42:11 -0500

    Tweaked resourceserver/client definitions

--- apis-authorization-server-war/src/main/resources/db/migration/mysql_content/V2__voltrondev.sql
+++ apis-authorization-server-war/src/main/resources/db/migration/mysql_content/V2__voltrondev.sql
@@ -5,21 +5,28 @@
 REPLACE INTO resourceserver
 (id,creationDate,modificationDate,contactEmail,contactName,description,resourceServerKey,resourceServerName,owner,secret,thumbNailUrl)
 VALUES
-(99997,current_date,current_date,'sysops@utest.com','uTest Sysops','voltrondev.utest.com','8b47c730-58c8-4a72-bd6b-f6a2769dd104','voltrondev.utest.com','sysops@utest.com','90654a91-938a-4a98-a7ab-d0da54f42d78',NULL);
+(99997,current_date,current_date,'sysops@utest.com','uTest Sysops','voltrondev.utest.com','voltrondev.utest.com','voltrondev.utest.com','sysops@utest.com','wQs!xzx9=',NULL);
 
 REPLACE INTO ResourceServer_scopes
 VALUES
+(99997, 'API'),
+(99997, 'SSO');
+
+REPLACE INTO resourceserver
+(id,creationDate,modificationDate,contactEmail,contactName,description,resourceServerKey,resourceServerName,owner,secret,thumbNailUrl)
+VALUES
+(99998,current_date,current_date,'sysops@utest.com','uTest Sysops','securitytest.utest.com','securitytest.utest.com','securitytest.utest.com','sysops@utest.com','O8^jsdw1',NULL);
+
+REPLACE INTO ResourceServer_scopes
+VALUES
+(99998, 'SSO'),
 (99997, 'API');
 
 REPLACE INTO client
 (id,creationDate,modificationDate,clientId,contactEmail,contactName,description,expireDuration,clientName,allowedImplicitGrant,allowedClientCredentials,secret,skipConsent,includePrincipal,thumbNailUrl,useRefreshTokens,resourceserver_id)
 VALUES
-(99997,current_date,current_date,'voltrondev.utest.com','sysops@utest.com','uTest Sysops','Voltron UI (voltrondev.utest.com)','86400','voltrondev.utest.com',1,0,NULL,0,1,NULL,0,99997);
+(99997,current_date,current_date,'browser','sysops@utest.com','uTest Sysops','browser','86400','browser',1,0,NULL,0,1,NULL,0,99997);
 
 REPLACE INTO Client_scopes
 VALUES
-(99997, 'API');
-
-REPLACE INTO Client_redirectUris (CLIENT_ID, element)
-VALUES
-(99997, 'https://voltrondev.utest.com/wsgi/api/auth/redirect');
+(99997, 'SSO');
